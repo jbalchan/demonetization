45b1b83afe59200f0a3271fbd70156258fb085ef
jeyassri balachandran <shreeju@users.noreply.github.com>
2017-02-26 18:56:20 +0530

    Update and rename TSA.R to TimeSeriesAnalysis.R

--- TimeSeriesAnalysis.R
+++ TimeSeriesAnalysis.R
@@ -1,32 +1,39 @@
+# import packages
 library(ggplot2)
 library(plyr)
-#,col.names = optnames
-#"%a, %d %b %Y %H:%M:%S %z",
-dt <- 10 #10 mins
 
-#optnames <- c("sno","text","favtd" ,"favcount","replysn", "date","trunc","replysid","id","replyuid","statussrc","screenname","rtcount", "isrt","rtd")
-tweetsjsr <- read.csv("D:/1 DATA BYTE/1_projects/demonetization proj/from shivani/demonetization-tweets.csv")
-#tweets <- unique(read.table("D:/1 DATA BYTE/1_projects/demonetization proj/from shivani/demonetization-tweets.csv", sep="\t", quote="", comment.char="",stringsAsFactors=FALSE, header=TRUE, nrows=8000))
+# set delta as 10 mins
+dt <- 10 
+
+# Read the dataset into a variable
+tweetsjsr <- read.csv("D:/1 DATA BYTE/1_projects/demonetization proj/from shivani/DataSets/demonetization-tweets.csv")
+# remove duplicate records
 tweets <- unique(tweetsjsr)
+# name the columns
 names(tweets) <- c("sno","text","favtd" ,"favcount","replysn", "date","trunc","replysid","id","replyuid","statussrc","screenname","rtcount", "isrt","rtd")
 
-#names(tweets) <- c("id", "date", "user", "text")
-#posixformt <- strptime(tweets$date,format = "%Y-%m-%d %H:%M")
-#tweets$date <- as.POSIXct(strptime(tweets$date,  tz = "GMT", format = "%Y-%m-%d %H:%M:%S"))
+# convert date from factors form to date format
 tweets$date <- as.POSIXct(tweets$date,  tz = "GMT", format = "%d-%m-%Y %H:%M")
 
-#cutoff later if necessary
+# Build datebreaks
+# find the first tweet
 minDate <- min(tweets$date)
+# find the last tweet
 maxDate <- max(tweets$date) + 60 * dt
+# set the breaks for time series analysis
 dateBreaks <- seq(minDate, maxDate, by=60 * dt)
 
+# Use hist to count the number of tweets per bin but don't plot.
 tweetCount <- hist(tweets$date, breaks=dateBreaks, plot=FALSE)
+# Strip out the left endpoint of each bin.
 binBreaks <- tweetCount$breaks[1:length(tweetCount$breaks)-1]
+# Count number of unique tweeters per bin.
 userCount <- sapply(binBreaks, function(d) length(unique(tweets$user[which((tweets$date >= d) & (tweets$date <= d + 60*dt))]))) 
+# creating data to be plotted
 plotData <- data.frame(dates=dateBreaks[1:length(dateBreaks)-1], tweets=as.numeric(tweetCount$count), users=as.numeric(userCount))
+# customizing the plot
 ggplot(plotData) + 
   geom_bar(aes(x=dates, y=tweets, color=users), stat="identity") +
   scale_x_datetime("Date") +
   scale_y_continuous("Number of tweets") +
   labs(title = "Number of tweets and unique users : #demonetization")
-  #opts(title="Number of tweets and unique users : #demonetization")
